081d98b25684702228cc4af32e4af9088845c282
dinesh123-dinesh <67948683+dinesh123-dinesh@users.noreply.github.com>
2023-09-03 00:34:21 +0530

    Add files via upload
    Updated Download Button

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-// JavaScript to toggle navigation menu on small screens
-const menuToggle = document.querySelector('.menu-toggle');
-const navigationMenu = document.querySelector('.navigation-menu .menu');
-
-menuToggle.addEventListener('click', () => {
+// Function to toggle the navigation menu on small screens
+function toggleMenu() {
+    const navigationMenu = document.querySelector('.navigation-menu .menu');
     navigationMenu.classList.toggle('show');
-});
+}
 
-// JavaScript for smooth scrolling when clicking on navigation links
-document.querySelectorAll('.menu a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        const targetId = this.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
-
+// Function to handle smooth scrolling
+function scrollToSection(targetId) {
+    const targetSection = document.getElementById(targetId);
+    if (targetSection) {
+        const navHeight = document.querySelector('.navigation-menu').offsetHeight;
         window.scrollTo({
-            top: targetSection.offsetTop - document.querySelector('.navigation-menu').offsetHeight,
+            top: targetSection.offsetTop - navHeight,
             behavior: 'smooth'
         });
+    }
+}
+
+// Event listener for menu toggle button
+const menuToggle = document.querySelector('.menu-toggle');
+if (menuToggle) {
+    menuToggle.addEventListener('click', toggleMenu);
+}
+
+// Event listeners for navigation links
+const navigationLinks = document.querySelectorAll('.menu a');
+navigationLinks.forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        scrollToSection(targetId);
     });
 });
-
-
-
-
-
